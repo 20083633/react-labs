e30a7a87b8b3402d765c7c81c5b0a51ebb47390e
20083633 <20083633@users.noreply.github.com>
2021-04-16 15:44:19 +0100

    Week 10 lab finished with Error

--- mern-lab-1/frontend/src/axios-orders.js
+++ mern-lab-1/frontend/src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://pizzapal-23344-default-rtdb.europe-west1.firebasedatabase.app/'
+    baseURL: 'http://localhost:5000/api'
 });
 
 export default instance;
